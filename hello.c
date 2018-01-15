50389a5deffdd0299873f0e8bb739dd4edf8812c
Hafsah <p16170123@my365.dmu.ac.uk>
2018-01-15 14:33:51 +0000

    Added junk1.txt

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+   // printf() displays the string inside quotation
+   printf("Fare thee well!/n");
+   printf("Hello World/n");
+   return 0;
+}
+
+
+git config --global user.email "p16170123@my365.dmu.ac.uk"
+  git config --global user.name "Hafsah"
